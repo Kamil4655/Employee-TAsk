481636a86248895994e06e9cceb55840bc9049ec
KaMiL374346363 <kamil.qehreman002@gmail.com>
2024-04-04 07:31:04 +0400

    Employeee

---
+++ Employee/Models/Assistant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Models
+{
+    internal class Assistant : Manager
+    {
+        public void GetFeedback(Employee employee)
+        {
+            if (employee.IsSuccessfull)
+            {
+                employee = GetPromotion(employee);
+
+                Console.WriteLine(employee.Salary + ": new salary");
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+        }
+    }
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,18 @@
+﻿using Employee.Models;
+
+namespace Employee
+{   
+    internal class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("Kamil", true, 555m);
+
+            Assistant assistant = new Assistant();
+
+            Manager manager = new Manager();
+
+            assistant.GetFeedback(employee);
+        }
+    }
+}
